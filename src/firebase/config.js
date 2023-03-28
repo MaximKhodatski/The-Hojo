f1679c16c57de4f9bcacd2d5d1c652a32a5217be
MaximKhodatski <maximaximax30@gmail.com>
2023-03-28 21:27:27 +0300

    sign up and storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDvK2yG74WnWDiixff5XVSvn7W0DAxmR_Y",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp 
 const timestamp = firebase.firestore.Timestamp 
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp }
